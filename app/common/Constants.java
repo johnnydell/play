96dbdfc08a46bdda32006b6552975ea9f3a20834
johnnydell <dzhjohnny@126.com>
2017-11-27 11:40:59 +0800

    asdf

--- app/common/Constants.java
+++ app/common/Constants.java
@@ -2,7 +2,7 @@ package common;
 
 public class Constants {
 	
-	public static String CTX_PATH = "";
+	public static String CTX_PATH = "2334";
 	
 	public static String INDEX_PATH = CTX_PATH+ "/edashboard/views/index.html";
 
