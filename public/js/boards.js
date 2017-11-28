bcc270055431dee27e2340247c7122fead289aaf
johnnydell <dzhjohnny@126.com>
2017-11-28 11:42:28 +0800

    commit

--- public/js/boards.js
+++ public/js/boards.js
@@ -3,14 +3,22 @@ var boards = function(){
 		var ractive = new Ractive({
 			el: ".container",
 			template: "#main-template",
-			data: {},
+			data: {root:manager.root},
 			onrender: function(){
 				manager.loadProperties(this, "boards", "../");
 			},
-			oncomplete: function(){
-				console.log("sdfsdf");
+			oncomplete: function(){						
 			}
 		});
+		
+		
+		ractive.on({
+			openExcel:function(){
+				var openDocObj; 
+				openDocObj = new ActiveXObject("SharePoint.OpenDocuments.1"); 
+				openDocObj.ViewDocument(manager.root+"/files/2.8_layout布局图.xls"); 
+			}
+		})
 	}
 	
 	return {
