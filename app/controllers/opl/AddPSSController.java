71c3fae7ba514ca094818695bb86f59edaef48b0
johnnydell <dzhjohnny@126.com>
2017-12-11 09:37:20 +0800

    adjust package of addPSS

--- app/controllers/opl/AddPSSController.java
+++ app/controllers/opl/AddPSSController.java
@@ -1,19 +1,14 @@
-package controllers;
+package controllers.opl;
 
 import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.alibaba.fastjson.JSONObject;
-
 import common.Constants;
 import common.util.FileUtil;
-import models.UploadFile;
-import models.UploadFileHistory;
 import play.mvc.Controller;
 import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.MultipartFormData.FilePart;
