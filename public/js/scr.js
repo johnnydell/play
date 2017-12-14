2bc7bd0c27c19886638dbab821b66ccccbe91c65
johnnydell <dzhjohnny@126.com>
2017-12-14 14:27:14 +0800

    adjust A: display board:

--- public/js/scr.js
+++ public/js/scr.js
@@ -1,5 +1,6 @@
 var scr = function(){
 	function init(){
+		var data;
 		var ractive = new Ractive({
 			el: ".container",
 			template: "#main-template",
@@ -9,9 +10,32 @@ var scr = function(){
 				manager.setMenuBar("scr");
 			},
 			oncomplete: function(){	
-			    
+				data = {
+						currType:'8888888888-2Y8',
+						currJP:'15',
+						currRS:'12',
+						planCC:'500',
+						actualCC:'501',
+						diff:'+1',
+						oee:'85%',
+						nextType:'6666666666-6SB',
+						nextJP:'15',
+						nextRS:'12'
+				}
+				
+				this.set("data",data);
+				
 			}
 		});
+		
+		ractive.on({
+			toShowEditor:function(e){
+				$(e.node).hide().next().show().focus();
+			},
+			toHideEditor:function(e){
+				$(e.node).hide().prev().text($(e.node).val()).show();
+			}
+		})
 	}
 	
 	return {
