e05b2a872e1bbba54c22f712f418a6f5dbc62c9f
johnnydell <dzhjohnny@126.com>
2017-12-16 15:45:52 +0800

    add script bat

---
+++ startup.bat
@@ -0,0 +1,9 @@
+@echo off
+set PLAY_HOME=D:\play-2.2.6
+set JAVA_HOME=D:\Program Files\Java\jdk1.7.0_79
+set PATH=%JAVA_HOME%/bin;%JAVA_HOME%/jre/bin;%PLAY_HOME%;
+set CLASSPATH=.;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools.jar; 
+echo %PATH%
+java -version
+play "run 8090"
+pause 
