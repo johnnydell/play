dc325279c17e144cde272622ac32294e345de623
johnnydell <dzhjohnny@126.com>
2018-01-22 11:27:45 +0800

    adjust lines and index page

--- public/js/lines.js
+++ public/js/lines.js
@@ -1,38 +1,87 @@
-var lines = function(){
-	var lineData;
+var lines = function(){	
 	var ractive = null;
+	var recList = [];
+	var rogList = [];
+	var stgList = [];
+	var fagList = [];
 	function init(){
 		ractive = new Ractive({
 			el: ".container",
 			template: "#main-template",
-			data: {},
+			data: {root:manager.root},
 			onrender: function(){
 				manager.loadProperties(this, "lines", "../");
 			},
 			oncomplete: function(){
-				initLineData();
+				formList();
+				ractive.set("recList",recList);
+				ractive.set("rogList",rogList);
+				ractive.set("stgList",stgList);
+				ractive.set("fagList",fagList);
 			}
 		});
 		
 		ractive.on({
 		   kpiplEvt:function(){
 		     location.href = "kpi.html";
+		   },
+		   gotoBoards:function(e){
+			   console.log(e.context);
+			   window.location.href=manager.root+"/views/boards.html?lineName="+e.context.line_name+"&lineId="+e.context.line_id;
 		   }
+		})
+	}
+	
+	function formList(){
+		var lineData = initLineData();
+		$.each(lineData,function(i,n){
+			var fag_type = n.lineName.toLowerCase().indexOf("fag");
+			var rec_type = n.lineName.toLowerCase().indexOf("rec");
+			var rog_type = n.lineName.toLowerCase().indexOf("rog");
+			var stg_type = n.lineName.toLowerCase().indexOf("stg");
+			if(fag_type != -1){
+				if(fagList.length == 4){
+					fagList.push({line_id:0,line_name:''});
+				}
+				fagList.push({line_id:n.id,line_name:n.lineName});
+			}
+			
+			if(rec_type != -1){
+				if(recList.length == 4){
+					recList.push({line_id:0,line_name:''});
+				}
+				recList.push({line_id:n.id,line_name:n.lineName});
+			}
+			
+			if(rog_type != -1){
+				if(rogList.length == 4){
+					rogList.push({line_id:0,line_name:''});
+				}
+				rogList.push({line_id:n.id,line_name:n.lineName});
+			}
+			
+			if(stg_type != -1){
+				if(stgList.length == 4){
+					stgList.push({line_id:0,line_name:''});
+				}
+				stgList.push({line_id:n.id,line_name:n.lineName});
+			}
 		})
 	}
 	
 	//取得默认结构数据
 	function initLineData(){
+		var ret;
 		$.ajax({
 			url		: manager.root + '/sysConfig/getProductLine',
 			type	: 'GET',
 			data	: '',
 			async   : false,
 			success: function(listdata){
-				lineData = listdata;
-				ractive.set("lineData",lineData);
+				ret = listdata;
 			}
     	});
+		return ret;
 	}
 	
 	return {
